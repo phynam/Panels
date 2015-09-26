38fb8346363d5a4aa5e3656c9845388ad430c092
pieter@hynam.net <Nineteen84!>
2015-09-26 18:30:01 -0400

    Made sidebars fixed position and changed scroll settings.

--- dist/js/panels.js
+++ dist/js/panels.js
@@ -14,13 +14,13 @@
 
         this.close = function(panel) {
             var $panel = $(_panel_class_prefix + panel);
-            _$panels.removeClass('is_' + panel + '_active');
+            _$panels.removeClass('is_active is_' + panel + '_active');
             $panel.attr('aria-hidden', 'true');
             _active_panel = undefined;
         };
 
         this.close_active = function() {
-            _$panels.removeClass('is_' + _active_panel + '_active');
+            _$panels.removeClass('is_active is_' + _active_panel + '_active');
             _$panels_main.removeClass('is_disabled');
         };
 
